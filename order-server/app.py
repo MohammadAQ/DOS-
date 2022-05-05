52e44caa7df55c0f1a098589980de26c3da71863
MohammadAQ <92224666+MohammadAQ@users.noreply.github.com>
2022-05-06 02:43:51 +0300

    Add files via upload
    the order server done by mohammad.

---
+++ order-server/app.py
@@ -0,0 +1,14 @@
+# Import application instance and port
+from flask_app import app, port
+####################################################################
+# Import error handler 
+import errorhandlers
+####################################################################
+# Import  routes
+import routes
+####################################################################
+
+# Run Flask application instance
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=port)
+####################################################################
