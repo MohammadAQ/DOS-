897959ee697fdb802852f2ddc21841eac91fd18b
yousefdheede <58173109+yousefdheede@users.noreply.github.com>
2022-05-05 21:06:20 -0400

    Add files via upload

--- catalog-server/book.py
+++ catalog-server/book.py
@@ -1,39 +1,41 @@
-from database import db, marshmallow, database_init
-############################################################################
 
-# Define the Book class that overrides SQLAlchemy's Model class
+
+#import database
+from database import db, marshmallow, database_init
+###########
+
+######### Define the Book class that overrides SQLAlchemy's Model class
 class Book(db.Model):
-    # Define the Book fields which will be mapped to database columns
+    ##############define database atttributes which will be mapped todatabase columns
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(200), nullable=False,)
     topic = db.Column(db.String(200), nullable=False,)
     quantity = db.Column(db.Integer, nullable=False, default=0,)
     price = db.Column(db.Float, nullable=False)
-############################################################################
-    # Replication field for consistency
+#########
+  ##### Replication field to creat  consistency
     sequence_number = db.Column(db.Integer, nullable=False, default=0,)
 ############################################################################
-    # Constructor
+   #######creeate data base Constructor
     def __init__(self, title, topic, quantity, price):
         self.title = title
         self.quantity = quantity
         self.topic = topic
         self.price = price
         self.sequence_number = 0
-############################################################################
-    # Static method to search for books based on the topic
+##############
+  ########## search for books based on book topic
     @classmethod
     def search(cls, topic):
-        # Returns books that contain the query string, ignoring case
+  ############# Returns the book that contain the query string
         return Book.query.filter(Book.topic.ilike(f'%{topic}%'))
-############################################################################
-    # Static method to get a book using its ID
+#################
+   ######### get a book using  ID
     @classmethod
     def get(cls, id):
         return Book.query.get(id)
-############################################################################
-    # Static method to update the fields of a book given its ID
-    # If the field is not passed (or passed as None), it will not be affected
+###############
+ ########update the fields of a book given  ID
     @classmethod
     def update(cls, id, title=None, quantity=None, topic=None, price=None, sequence_number=None):
         book = Book.query.get(id)
@@ -43,8 +45,8 @@ class Book(db.Model):
         book.quantity = quantity if quantity is not None and quantity >= 0 else book.quantity
         book.topic = topic if topic is not None else book.topic
         book.price = price if price is not None and price >= 0.0 else book.price
-############################################################################
-        # Update sequence number of book
+##########
+     #####Update sequence number
         if sequence_number is None:
             if title is not None or quantity is not None or topic is not None or price is not None:
                 book.sequence_number = book.sequence_number + 1
@@ -53,56 +55,55 @@ class Book(db.Model):
 
         db.session.commit()
         return book
-############################################################################
-    # Dump method to view all rows
+#############
+######### view all rows
     @classmethod
     def dump(cls):
         return Book.query.all()
 
-############################################################################
+################
 # Add the 7 books as an initial entry to the database
 database_init += [
     Book('How to get a good grade in DOS in 40 minutes a day', 'Distributed Systems', 12, 10.00),
     Book('RPCs for Noobs', 'Distributed Systems', 5, 50.00),
     Book('Xen and the Art of Surviving Undergraduate School', 'Graduate School', 13, 10.00),
     Book('Cooking for the Impatient Undergrad', 'Graduate School', 55, 10.00),
-    Book('How to finish Project 3 on time', 'University Problems', 40, 10.00),
-    Book('Why theory classes are so hard', 'University Problems', 38, 10.00),
-    Book('Spring in the Pioneer Valley', 'Developer Life', 15, 10.00),
+    Book('How to finish Project 3 on time', 'new arrival', 40, 10.00),
+    Book('Why theory classes are so hard', 'new arrival', 38, 10.00),
+    Book('Spring in the Pioneer Valley', 'new arrival', 15, 10.00),
 ]
 
-############################################################################
-# Define Marshmallow Formatter Schema class for query-by-topic response fields
+###############
+###### Marshmallow  Schema for query-by-topic
 class TopicSchema(marshmallow.Schema):
     class Meta:
         fields = ('id', 'title', 'topic')
 
-############################################################################
-# Define Marshmallow Formatter Schema class for query-by-item response fields
+###################
+######### Marshmallow  Schema  for query-by-item
 class ItemSchema(marshmallow.Schema):
     class Meta:
         fields = ('title', 'quantity', 'price')
 
-############################################################################
-# Define Marshmallow Formatter Schema class for update response fields
+########
+####### Marshmallow  Schema  for update
 class UpdateSchema(marshmallow.Schema):
     class Meta:
         fields = ('title', 'quantity', 'topic', 'price')
 
-############################################################################
-# Define Marshmallow Formatter Schema class for replication consistency:
+#################
+####### Marshmallow  Schema class for replication consistency
 class ReplicationSchema(marshmallow.Schema):
     class Meta:
         fields = ('sequence_number', 'title', 'quantity', 'topic', 'price')
-
-############################################################################
-# Instantiate an object from each schema class
+#################
+######## create an object from each schema
 item_schema = ItemSchema()
 topic_schema = TopicSchema(many=True)
 update_schema = UpdateSchema()
 replication_schema = ReplicationSchema()
 
-############################################################################
+################
 # Dump
 class DumpSchema(marshmallow.Schema):
     class Meta:
@@ -111,4 +112,4 @@ class DumpSchema(marshmallow.Schema):
 
 dump_schema = DumpSchema(many=True)
 
-############################################################################
+##############
