a7a5c4bb4c0599fa65528e21b9fea7069f2e7a0c
yousefdheede <58173109+yousefdheede@users.noreply.github.com>
2022-05-05 21:34:51 -0400

    Add files via upload

--- catalog-server/flask_app.py
+++ catalog-server/flask_app.py
@@ -1,10 +1,10 @@
 from flask import Flask
 from os import environ
-############################################################################
-# Flask application instance
+##########
+#####Flask application instance
 app = Flask(__name__)
-############################################################################
-# Get addresses of front end and order servers from the environment variables
+#######################
+#### addresses of front  and order servers from the environment variables
 ORDER_ADDRESS = environ.get('http://192.168.1.11:5000 | http://192.168.1.16:5000')
 FRONT_END_ADDRESS = environ.get('http://192.168.1.15:5000')
 CATALOG_ADDRESSES = environ.get('http://192.168.1.13:5000 | http://192.168.1.17:5000')
@@ -14,13 +14,13 @@ else:
     CATALOG_ADDRESSES = CATALOG_ADDRESSES.split('|')
     
     
-############################################################################
-# Get the application port from the environment variables
+##################
+###Get port from the environment variables
 port = int(environ.get('PORT', 5000))
     
 
-############################################################################
-# Get the flask environment settings from the environment variables
+################################
+######flask environment settings from  environment variables
 app.config['development'] = environ.get('development')
 app.config['True'] = bool(environ.get('True'))
 
